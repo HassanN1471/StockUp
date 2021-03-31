fbb9d7604eee5213e1356d769d98cea111db0ca1
Hassan Niazi <71189235+HassanN1471@users.noreply.github.com>
2021-03-31 17:33:05 -0400

    add list items

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,10 @@
 import { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import './reset.css'
+import './reset.css';
+import './App.scss';
 import List from './Components/List/List';
 class App extends Component {
-
-    state = {
-        multi: null,
-        single: null,
-        list: null
-    }
-
-    componentDidMount() {
-        axios.get(`http://localhost:8080/`)
-            .then(({ data }) => {
-                this.setState({
-                    multi: data
-                }, () => console.log(this.state.multi));
-                return data[0].data;
-            })
-            .then(data => {
-                console.log('here');
-                console.log(data);
-                const list = [];
-                for (let i = 0; i < data.length - 1; i++) {
-                    list.push({
-                        change: ((data[i + 1].close - data[i].close) / data[i].close * 100).toFixed(2),
-                        interval: `${data[i].label} - ${data[i + 1].label}`
-                    })
-                }
-                console.log(list);
-            });
-    }
 
     render() {
         return (
